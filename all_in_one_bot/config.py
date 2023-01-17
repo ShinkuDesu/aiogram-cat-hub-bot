8ceff0bd2f73de405126c8e77b22820de085a33a
shinku_desu <dimaxihay@gmail.com>
2023-01-17 15:07:51 +0300

    some new

--- all_in_one_bot/config.py
+++ all_in_one_bot/config.py
@@ -5,6 +5,7 @@ HELP_MSG = '''Commands:
 /delete [msg]- delete message
 /random [int] - generate random text
 /adwice - get a good adwice
+/cat - get a funny cat img
 '''
 
 ADVICES = [
@@ -20,4 +21,15 @@ ADVICES = [
     'My personal recommendation would be to improve your basic language skills before going abroad.',
     'I would always recommend that everyone use paper bags rather than plastic ones.',
     'Youd better not eat so much spicy food or you will get heartburn.',    
+]
+
+CATS_IMGS = [
+    'https://i.pinimg.com/564x/f5/4f/13/f54f13638f0941531c46b817022065bb.jpg',
+    'https://avatars.dzeninfra.ru/get-zen_doc/3985746/pub_637c7c05d7971e0d378ae59f_637f1aa019398772ed547f6d/scale_1200',
+    'https://kotsf.ru/wp-content/uploads/2020/02/f222ce3e08376ced9dae7f03557091ba.jpg',
+    'https://cs8.pikabu.ru/post_img/big/2018/01/23/7/1516702880178892189.jpg',
+    'https://www.meme-arsenal.com/memes/871ad0cf10895c4cb8cf07d1afea93c9.jpg',
+    'https://kaifolog.ru/uploads/posts/2013-03/thumbs/1363239460_010.jpg',
+    'https://www.rulez-t.info/uploads/posts/2020-05/1590900680_smeshnye-koty-1.jpg',
+    'https://i.ytimg.com/vi/M-XtB0R3ri4/maxresdefault.jpg',
 ]
--- all_in_one_bot/main.py
+++ all_in_one_bot/main.py
@@ -9,10 +9,18 @@ bot = am.Bot(TOKEN_API_KEY)
 dp = am.Dispatcher(bot)
 
 
+kb = am.types.ReplyKeyboardMarkup(
+    resize_keyboard=True,
+)
+cat_btn = am.types.KeyboardButton('/cat')
+advice_btn = am.types.KeyboardButton('/advice')
+kb.add()
+
+
 
 @dp.message_handler(commands=['help', 'start'])
 async def help_cmd(message: am.types.Message):
-    await message.answer(cf.HELP_MSG)
+    await message.answer(cf.HELP_MSG, reply_markup=kb)
 
 
 @dp.message_handler(commands=['reply'])
@@ -33,6 +41,13 @@ async def delete_cmd(message: am.types.Message):
 @dp.message_handler(commands=['adwice'])
 async def advice_cmd(message: am.types.Message):
     await message.answer(random.choice(cf.ADVICES))
+    await message.delete()
+
+
+@dp.message_handler(commands=['cat'])
+async def cat_cmd(message: am.types.Message):
+    await message.answer_photo(random.choice(cf.CATS_IMGS))
+    await message.delete()
 
 
 @dp.message_handler(commands=['random'])
@@ -52,4 +67,8 @@ async def on_startup(_):
     print('Bot is running')
 
 if __name__ == '__main__':
-    am.executor.start_polling(dp, on_startup=on_startup)
+    am.executor.start_polling(
+        dp,
+        on_startup=on_startup,
+        skip_updates=True,
+    )
