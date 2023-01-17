4f29f7a16e2642275b8aab71ca684d8db1536f35
shinku_desu <dimaxihay@gmail.com>
2023-01-17 13:28:08 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NFT-parsers
+
+My parsers for some NFT markerplaces.
+
+Download and put the chromedriver in the root of the project to work.
---
+++ bot_1/main.py
@@ -0,0 +1,8 @@
+import aiogram
+
+
+TOKEN_API_KEY = '5754426530:AAE7vnFyukLnw8YKXwZZ_XffGYcmPBvJL1g'
+
+
+bot = aiogram.Bot(TOKEN_API_KEY)
+dp = aiogram.Dispatcher(bot)
